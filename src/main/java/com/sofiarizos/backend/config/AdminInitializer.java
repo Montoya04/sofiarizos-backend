fd360c29a70b9859c4c9a95d0ab6435a6634b7fb
Montoya04 <alejandromontoya412@gmail.com>
2026-01-16 15:34:27 -0500

    create admin automatically

---
+++ src/main/java/com/sofiarizos/backend/config/AdminInitializer.java
@@ -0,0 +1,37 @@
+package com.sofiarizos.backend.config;
+
+import com.sofiarizos.backend.model.Admin;
+import com.sofiarizos.backend.repository.AdminRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AdminInitializer {
+
+    @Bean
+    CommandLineRunner initAdmin(
+            AdminRepository adminRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+
+            String email = "admin@admin.com";
+
+            if (adminRepository.findByEmail(email).isEmpty()) {
+
+                Admin admin = new Admin();
+                admin.setEmail(email);
+                admin.setPassword(passwordEncoder.encode("123456"));
+                admin.setActivo(true);
+
+                adminRepository.save(admin);
+
+                System.out.println("✅ ADMIN CREADO AUTOMÁTICAMENTE");
+            } else {
+                System.out.println("ℹ️ ADMIN YA EXISTE");
+            }
+        };
+    }
+}
