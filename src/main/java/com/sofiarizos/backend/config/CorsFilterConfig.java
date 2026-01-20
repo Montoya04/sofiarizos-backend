fa6edbbe31a9fc63db86920a9f0289866d56752a
Montoya04 <alejandromontoya412@gmail.com>
2026-01-20 11:46:02 -0500

    CorsFilterConfig.java

---
+++ src/main/java/com/sofiarizos/backend/config/CorsFilterConfig.java
@@ -0,0 +1,35 @@
+package com.sofiarizos.backend.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CorsFilterConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal (
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        
+        response.setHeader("Access-Control-Allow-Origin", "https://sofiarizos-frontend.vercel.app");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+    
+}
